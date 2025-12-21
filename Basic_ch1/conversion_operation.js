0e1e4ab13e12cc0e7098c393322a12719c559fde
Virendra117 <v3itechnology@gmail.com>
2025-12-21 09:00:25 +0000

    Conversion and Operation

--- Basic_ch1/conversion_operation.js
+++ Basic_ch1/conversion_operation.js
@@ -1,1 +1,21 @@
 console.log("******************Conversion & Operation in JacaScript ***************");
+
+let value=33
+let ConversionValue=Number(value);
+//console.log(ConversionValue);
+///////////////////////////////////////////////
+
+let str="33"
+let ConversionValueInNumber=Number(str);
+//console.log(ConversionValueInNumber);
+
+
+//==== Here Number Convert into String Datatype=======
+let intNumber=undefined
+let ConversionValueInString=String(intNumber)
+// console.log(ConversionValueInString);
+// console.log(typeof(ConversionValueInString));
+
+let conversionInBoolean=Boolean(intNumber);
+console.log(conversionInBoolean);
+
