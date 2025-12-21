34db9e32bfd4456a4af1aba871f8e5b9401e0351
Virendra117 <v3itechnology@gmail.com>
2025-12-21 11:05:59 +0000

    Compair value

---
+++ Basic_ch1/compairision.js
@@ -0,0 +1,19 @@
+console.log("Compair value in Java Srcipt");
+
+// console.log("6">2);
+// console.log(6>"2");
+
+
+// console.log(null > 0) // output false
+// console.log(null == 0) // output false
+// console.log(null >= 0) // output true
+
+// console.log(undefined > 0) // output false
+// console.log(undefined == 0) // output false
+// console.log(undefined >= 0) // output false
+
+
+
+/////// Strict Check (===)
+
+console.log("45" === 45) // output false
