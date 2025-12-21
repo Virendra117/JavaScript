e5ea0b31b033dc59c81cbb21900130e00d6b27d1
Virendra117 <v3itechnology@gmail.com>
2025-12-21 16:58:06 +0000

    datatypeSummary

---
+++ Basic_ch1/datatype_Summary.js
@@ -0,0 +1,21 @@
+// Primitive Datatype
+// 7 type : String, Number, Boolean, null, undefined, Symbol, BigInt.
+const str="V3i Technology"
+const counter=50
+const conterValue=23.98
+const isLoggedIn=true
+const IsWetherTemp=null
+const studentId;
+
+const Id=Symbol('9843')
+const anotherID=Symbol('9843')
+
+console.log(Id)
+//console.log(anotherID===Id)
+
+
+
+
+
+// Non Primitive / Reference type Datatype
+//Array, Object, Function
