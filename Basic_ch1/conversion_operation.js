f4e1ce55c8ba0e426bee9ef716f8ecbe6d74fbc1
Virendra117 <v3itechnology@gmail.com>
2025-12-21 10:51:15 +0000

    Conversion and Operation

--- Basic_ch1/conversion_operation.js
+++ Basic_ch1/conversion_operation.js
@@ -17,5 +17,50 @@ let ConversionValueInString=String(intNumber)
 // console.log(typeof(ConversionValueInString));
 
 let conversionInBoolean=Boolean(intNumber);
-console.log(conversionInBoolean);
+//console.log(conversionInBoolean);
+
+
+
+//************************Operation ********************/
+
+let val=55
+let convertVal= -val
+//console.log(convertVal);
+
+
+let str1="Virendra"
+let str2=" Sonwani"
+let strAdd=str1+str2;
+//console.log(strAdd);
+
+let num1=12
+let num2=15
+let totalSum=num1+num2;
+console.log(totalNum);
+
+/*
+    console.log(num1 + num2);
+    console.log(num1 - num2);
+    console.log(num1 * num2);
+    console.log(num1 / num2);
+    console.log(num1 % num2);
+
+    console.log(1+"5"); // which one first value, behave like that
+    //output : 15
+
+    console.log("1"+ 5); // which one first value, behave like that
+    //output : 15
+
+    console.log("1" +5 + 4); // which one first value, behave like that
+    //output : 154
+
+    console.log(1 +5 + "4"); // which one first value, behave like that
+    //output : 64
+    //Note : firstly add first two values means 1+5 will be added then third value contact.
+            like (1+5)+"4"= 64
+*/
+
+
+
+
 
