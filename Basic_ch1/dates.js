0e23b73ea5b887816d37e487aef3b81fe4c54ae7
Virendra117 <v3itechnology@gmail.com>
2025-12-22 10:56:43 +0000

    date done

---
+++ Basic_ch1/dates.js
@@ -0,0 +1,24 @@
+let myDate=new Date();
+//console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toDateString());
+
+//let myCreatedDate=new Date(2023,0,24)
+//let myCreatedDate=new Date(2023,0,24,7,8)
+let myCreatedDate=new Date("2023-01-24")
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toLocaleDateString());
+// console.log(myCreatedDate.toLocaleTimeString());
+
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp); // result gives in mili second
+
+
+
+
+
