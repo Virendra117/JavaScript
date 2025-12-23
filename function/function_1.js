e4ec2b593293f661cf70fc46b1e621af5aeb0eb7
Virendra117 <v3itechnology@gmail.com>
2025-12-23 13:39:13 +0000

    function1

---
+++ function/function_1.js
@@ -0,0 +1,14 @@
+// define of function
+function abc(){
+    console.log("Hello V3i Technology.")
+}
+
+// calling of function
+abc();
+/*
+Note: 
+    abc is name of function, it's also called reference and parathesis () is called execution.
+    abc => reference/function name
+    () => execution
+
+*/
