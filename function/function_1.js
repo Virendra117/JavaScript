31f0bcdacd9a212e4a55c6d8c8727cad6a165925
Virendra117 <v3itechnology@gmail.com>
2025-12-23 17:13:16 +0000

    function2

--- function/function_1.js
+++ function/function_1.js
@@ -12,3 +12,30 @@ Note:
     () => execution
 
 */
+
+
+function calculateCartItemPrice(num){
+    return num
+}
+
+// console.log(calculateCartItemPrice(200));
+
+///using spread concept
+function NewcalculateCartItemPrice(...num){
+    return num
+}
+// console.log(NewcalculateCartItemPrice(200,300,500));
+
+///////////sum of these number////////////
+let totalItemPrice=NewcalculateCartItemPrice(200,300,500)
+// console.log(totalItemPrice[0]+totalItemPrice[1]+totalItemPrice[2]);
+let result=0
+for(let i=0;i<totalItemPrice.length;i++)
+{
+     result+=parseInt(totalItemPrice[i])
+ 
+}
+ console.log(result);
+
+
+
