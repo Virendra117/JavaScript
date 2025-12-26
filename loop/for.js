f9c17f7206bf45d33279addb90b6ed60140ae451
Virendra117 <v3itechnology@gmail.com>
2025-12-26 14:36:10 +0000

    for done

---
+++ loop/for.js
@@ -0,0 +1,28 @@
+//loop for while, do-while, forof
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// while (condition) {
+    
+// }
+
+// do {
+    
+// } while (condition);
+
+//////////////////////////
+
+// ["","",""]
+// [{},{},{}]
+// for (const index of object) {
+    
+// }
+
+
+
+const  arr=[1,2,3,4,5,6]
+for (const index of arr) {
+    console.log(index)
+}
